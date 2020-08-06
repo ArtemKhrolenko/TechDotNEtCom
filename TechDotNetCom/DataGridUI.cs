373f2e112214c7707603cbf332d993555636709c
roninout <roninorgua@gmail.com>
2020-08-06 10:59:00 +0300

    test

--- TechDotNetCom/DataGridUI.cs
+++ TechDotNetCom/DataGridUI.cs
@@ -117,6 +117,11 @@ namespace DataGridAX
             gridControl2.Refresh();
         }
 
+        private void testMerge()
+        {
+            
+        }
+
         #endregion
 
         #region Регистрация класса в реестре
